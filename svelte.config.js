a99e183cf4dae7ba4bfbe970c10ad970c8f15948
Dalton Blake <daltonb0599@gmail.com>
2025-04-16 16:28:04 +0200

    Attempt two at ensuring csp

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,27 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({})
+		adapter: adapter({
+			headers: {
+				'/*': {
+					'Content-Security-Policy': 
+						`default-src 'self'; 
+						script-src 'self' https://kit.fontawesome.com; 
+						style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; 
+						img-src 'self' data: blob:; 
+						font-src 'self' https://fonts.gstatic.com; 
+						connect-src 'self'; 
+						frame-ancestors 'none'; 
+						base-uri 'self'; 
+						form-action 'self';`,
+					'X-Frame-Options': 'DENY',
+					'X-Content-Type-Options': 'nosniff',
+					'Referrer-Policy': 'strict-origin-when-cross-origin',
+					'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
+					'Permissions-Policy': 'geolocation=(), camera=(), microphone=()'
+				}
+			}
+		})
 	}
 };
 
