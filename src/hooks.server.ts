3ea88d76cf1ebc4bc84faff486225fe3206b9321
Dalton Blake <daltonb0599@gmail.com>
2025-04-16 22:19:36 +0200

    icon fix!!

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,7 +7,7 @@ const securityHeaders = {
     'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
     'Permissions-Policy': 'geolocation=(), camera=(), microphone=()',
     'Cross-Origin-Opener-Policy': 'same-origin',
-    'Cross-Origin-Embedder-Policy': 'require-corp',
+    'Cross-Origin-Embedder-Policy': 'cross-origin',
     'Cross-Origin-Resource-Policy': 'cross-origin',
     'Access-Control-Allow-Origin': '*'
 }
