5e04c5c00946697c8fccc09aef77384b32a77183
Dalton Blake <daltonb0599@gmail.com>
2025-06-19 20:23:07 +0200

    Updated to local fonts

--- svelte.config.js
+++ svelte.config.js
@@ -12,9 +12,9 @@ const config = {
 			directives: {
 				'default-src': ["'none'"],
 				'script-src': ["'self'", "https://kit.fontawesome.com/aabe3eec3e.js"],
-				'style-src': ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+				'style-src': ["'self'", "'unsafe-inline'", ""],
 				'img-src': ["'self'", "data:", "blob:", "https://www.netlify.com"],
-				'font-src': ["'self'", "https://fonts.gstatic.com", "https://ka-f.fontawesome.com"],
+				'font-src': ["'self'", "https://ka-f.fontawesome.com"],
 				'connect-src': ["'self'", "https://kit.fontawesome.com", "https://ka-f.fontawesome.com"],
 				'frame-ancestors': ["'none'"],
 				'base-uri': ["'self'"],
